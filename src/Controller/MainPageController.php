a903299d59a7176b1426be09926e8682beb895f4
LouiseVen <louise.venuleth@gmail.com>
2023-08-19 11:04:37 +0200

    main page

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\HotelRepository;
+use App\Repository\VeterinarianRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainPageController extends AbstractController
 {
     #[Route('/home', name: 'app_main_page')]
-    public function index(): Response
+    public function index(HotelRepository $hotelRepository, VeterinarianRepository $veterinarianRepository): Response
     {
 
-        $user=$this->getUser();
+        $user = $this->getUser();
+        $hotel = $hotelRepository->findAll();
+        $veterinarian = $veterinarianRepository->findAll();
         return $this->render('main_page/index.html.twig', [
             'user' => $user,
+            'hotels' => $hotel,
+            'veterinarians' => $veterinarian,
         ]);
     }
 }
