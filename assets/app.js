fced6462c65c9f58152d726e9fe7328a6445663d
LouiseVen <louise.venuleth@gmail.com>
2023-08-19 02:17:29 +0200

    Improvement of the style

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './styles/global.scss';
+import "./styles/app.css";
+import "./styles/global.scss";
+
+require("bootstrap");
